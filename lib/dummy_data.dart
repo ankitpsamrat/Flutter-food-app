e806251660d4a79f5aa93a14a5917f2e9e7326aa
ankitpsamrat <ankit.p.samrat@gmail.com>
2022-05-19 20:53:10 +0530

    first commit

---
+++ lib/dummy_data.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:food_app/models/catagory.dart';
+
+const DUMMY_CATAGORIES = [
+  const Catagory(id:'c1', title:'italian', color:Colors.purple,)
+];
---
+++ lib/models/catagory.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class Catagory {
+  final String id;
+  final String title;
+  final Color color;
+
+  Catagory(
+    this.id,
+    this.title,
+    this.color,
+  );
+}
